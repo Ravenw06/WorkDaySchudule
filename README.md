a0136ec83902974ca3a9f25b146421c899db6914
Your Name <you@example.com>
2021-04-16 18:23:17 -0400

    finislised

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Work Day Scheduler Starter Code
+
+#Work Day Scheduler
+***
+##It planner is use for operation management to plan out their daily schedule on a daily basis 
+
+1. When user open the planner
+- current day is displayed at the top of the calendar
+2. When user scroll down
+- their can see the time blocks row for each of their operation hour
+    - each time block is color coded to reflect the time blocks of 
+    | past(grey)| present(red) | future(green)
+3. when user click into the text area of selected time block
+- they are enter the planned event
+4. when user click save button beside the selected time block
+- the input in the selected row would be saved into local storage
+- the input will then be pulled back out from the local storage when user refresh the page
+- the data is saved storage until the user edit it
+
+<img src="assets\js\WSimage.JPG" width="250">
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,79 +1,93 @@
-var currentTimeEl = document.querySelector("#currentDay");
-currentTimeEl.textContent = moment().format('dddd, MMMM Do YYYY');
 // Adding current time to the header section
+var containerEl = $("#container");
 var currentHour = moment().hours();
-var endTime = 19; //closing time
-var containerEl = document.querySelector(".container");
-// select to the container id 
+var endTime = 24; //closing time
 
-function convertTo12Hour(time){
-    //time is an integer
-    if (time > 12) {
-      return time - 12;
-    } else {
-      return time
-    } 
+creatTimeRow();
+// getColor();
+// getData();
+
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+// $(".saveBtn").click();
+
+function creatTimeRow() {
+  var time = 15; //open time
+  while (time < endTime + 1) {
+    //as long as we are open
+    var holderEl = $("<form class='row time-block'>");
+
+    $("<div class='col-1 col-md-1 hour'>")
+      .text(convertTo12Hour(time))
+      .appendTo(holderEl);
+
+    $("<textarea  class='col-9 col-md-10 description'>")
+      .attr("data-hr", time)
+      .addClass(getColor(time))
+      .val(getData(time))
+      .appendTo(holderEl);
+
+    var saveBtn = $("<button class='saveBtn col-2 col-md-1'>");
+    saveBtn.appendTo(holderEl).click(setData);
+
+    $("<i class='fa fa-save'>").appendTo(saveBtn);
+
+    containerEl.append(holderEl);
+
+    time++;
+  }
 }
 
-function creatTimeRow () {
-    var time = 9;//open time
-    while (time < endTime + 1){ //as long as we are open
-    var holderEl = document.createElement("div");
-    holderEl.className = "row time-block";
-
-    var timeEl = document.createElement("div");
-    timeEl.className = "col-1 col-md-1 hour d-flex";
-    timeEl.textContent = convertTo12Hour(time);
-
-    var inputEl = document.createElement("textarea");
-    inputEl.className = "col-9 col-md-10 description"; 
-    inputEl.setAttribute("hr", time)
-    var saveBtn = document.createElement("button")
-    saveBtn.className = "saveBtn col-2 col-md-1 fa fa-save border rounded p-3";
-
-    holderEl.appendChild(timeEl);
-    holderEl.appendChild(inputEl);
-    holderEl.appendChild(saveBtn);
-    containerEl.appendChild(holderEl);
-    time ++;
-    console.log(time)
-    }
+function convertTo12Hour(hour) {
+  //time is an integer
+  //   if (time > 12) {
+  //     return time - 12;
+  //   } else {
+  //     return time;
+  //   }
+  return moment(hour, "H").format("ha");
 }
-creatTimeRow ();
 
+function getColor(hour) {
+  //   $(".description").each(function () {
+  //     var eachHour = parseInt($(this).attr("hr"));
+  //     if (eachHour < currentHour) {
+  //       $(this).addClass("past");
+  //     } else if (eachHour === currentHour) {
+  //       $(this).addClass("present");
+  //     } else {
+  //       $(this).addClass("future");
+  //     }
+  //   });
+  let className = "future";
 
-function changeColor() {
+  if (hour === currentHour) {
+    className = "present";
+  } else if (hour < currentHour) {
+    className = "past";
+  }
 
-    $(".description").each(function() {
-        var eachHour = parseInt($(this).attr("hr"));
-        if (eachHour < currentHour) { 
-            $(this).addClass("past");
-        }
-        else if (eachHour === currentHour) {
-            $(this).addClass("present");
-        }
-        else {
-            $(this).addClass("future");
-        }
-    });
-}    
-changeColor();
+  return className;
+}
 
-function getData(){
-    hour = $(this).siblings(".description").attr("hr");
-    description = $(this).siblings(".description").val();
-    localStorage.getItem(hour, description); 
-    $(".description").each(function() {
-        while ($(this).siblings(".description").attr("hr") = hour){
-        $(this).siblings(".description").val() = description;}    
-    })
-};
+function getData(hour) {
+  //   hour = $(this).siblings(".description").attr("hr");
+  //   description = $(this).siblings(".description").val();
+  //   localStorage.getItem(hour, description);
+  //   $(".description").each(function () {
+  //     while (($(this).siblings(".description").attr("hr") = hour)) {
+  //       $(this).siblings(".description").val() = description;
+  //     }
+  //   });
+  return localStorage.getItem(convertTo12Hour(hour));
+}
 
-$(".saveBtn").click(function() {
-    var hour = $(this).siblings(".description").attr("hr");
-    var description = $(this).siblings(".description").val();
-    localStorage.setItem(hour, description);
-});
+function setData(event) {
+  event.preventDefault();
 
+  var siblings = $(this).siblings();
 
-getData();
+  var hour = $(siblings[0]).text();
+  var textarea = $(siblings[1]).val();
+
+  localStorage.setItem(hour, textarea);
+}
